71975e1ef1e163b15ff830979ab6268261403e05
tobias.mueller@schriesheim.de <29426934+MuellerSchriesheim@users.noreply.github.com>
2018-08-20 08:49:40 +0200

    Added Get-iDoitReport

--- PSIdoit/functions/Get-iDoitReport.ps1
+++ PSIdoit/functions/Get-iDoitReport.ps1
@@ -1,4 +1,28 @@
 function Get-iDoitReport
+<#
+.SYNOPSIS
+    Receives a List of i-doit Reports or Runs the specified report
+.DESCRIPTION
+    Receives a List of i-doit Reports or Runs the specified report.
+
+    When the Cmdlet is called without -ReportID Parameter you'll get
+    a list of all reports in i-doit the api user has access.
+
+    When the Cmdlet is called with the ReportID the API will return
+    the result of those report
+.PARAMETER ReportID
+    The valid ID of an i-doit Report
+.EXAMPLE
+    PS C:\> Get-iDoitReport
+
+    Receives a list of all Reports the API User has access
+.EXAMPLE
+    PS C:\> Get-iDoitReport -ReportID 12
+
+    Runs Report with the ID 12 and receives the result of those report
+.OUTPUTS
+    [PSCustomObject]
+#>
 {
     param (
         $ReportID
