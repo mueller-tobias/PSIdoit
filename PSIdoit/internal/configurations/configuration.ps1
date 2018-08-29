760e7b4b2c0caed4ecd0c7d4e99628350c4b6f4f
tobias.mueller@schriesheim.de <29426934+MuellerSchriesheim@users.noreply.github.com>
2018-08-29 07:50:53 +0200

    Added Default Configuration

--- PSIdoit/internal/configurations/configuration.ps1
+++ PSIdoit/internal/configurations/configuration.ps1
@@ -10,3 +10,16 @@ feel totally free to split them into multiple files.
 # Example Configuration
 Set-PSFConfig -Module 'bConnect' -Name 'Example.Setting' -Value 10 -Initialize -Validation 'integer' -Handler { } -Description "Example configuration setting. Your module can then use the setting using 'Get-PSFConfigValue'"
 #>
+
+#Path to an Configuration JSON File
+$ConfigurationFile = "$($(Get-PSFConfig -Module 'Schriesheim-IT' -Name 'ConfigurationRepository').Value)\PSIdoit\PSIdoit.json"
+
+Set-PSFConfig -Module 'PSiDoit' -Name 'iDoit.ApiKey' -Value '' -Initialize -Validation 'String' -Handler { } -Description "API Key der für die Verbindung zur REST API benötigt wird." -ModuleExport
+Set-PSFConfig -Module 'PSiDoit' -Name 'iDoit.User' -Value '' -Initialize -Validation 'String' -Handler { } -Description "Benutzername der für die Verbindung zur REST API benötigt wird." -ModuleExport
+Set-PSFConfig -Module 'PSiDoit' -Name 'iDoit.Server' -Value '' -Initialize -Validation 'String' -Handler { } -Description "Server auf die iDoit REST API ausgeführt wird." -ModuleExport
+
+
+If ($ConfigurationFile)
+{
+    Import-PSFConfig -Path $ConfigurationFile
+}
